2d656a5851f46be1082976e9dd0a894209ef1793
Jardine <jardine.miller@gmail.com>
2018-03-20 14:48:27 +0000

    Added block.js

---
+++ Block.js
@@ -0,0 +1,21 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+
+    constructor(timestamp, transactions, previousHash = '') {
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.transactions = transactions;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+
+    }
+
+}
+
+module.exports = Block;
--- blockchain.js
+++ blockchain.js
@@ -1,2 +1,5 @@
 const SHA256 = require('crypto-js/sha256');
 
+
+
+module.exports = Blockchain;
